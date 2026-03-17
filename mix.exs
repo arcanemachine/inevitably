4d5dc32a546fecc0dcea8db69653cbba4cc03f3c
Nicholas Moen <arcanemachine@gmail.com>
2026-03-16 21:12:02 -0600

    chore: support Elixir versions >=1.15

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Inevitably.MixProject do
     [
       app: :inevitably,
       version: @version,
-      elixir: "~> 1.19",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
 
