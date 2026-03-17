943a2642f358d54712d241539a01d305b70aba5d
Nicholas Moen <arcanemachine@gmail.com>
2026-03-16 21:04:39 -0600

    chore: add hex package metadata and source link

--- mix.exs
+++ mix.exs
@@ -1,28 +1,45 @@
 defmodule Inevitably.MixProject do
   use Mix.Project
 
+  @project_name "Inevitably"
+  @source_url "https://github.com/arcanemachine/inevitably"
+  @version "0.1.0"
+
   def project do
     [
       app: :inevitably,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Hex
+      description:
+        ~s(Inevitably provides an ergonomic "eventually" do...end macro for ExUnit tests.),
+      package: package(),
+
+      # Docs
+      name: @project_name,
+      source_url: @source_url
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  defp package do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      name: :inevitably,
+      files: ~w(.formatter.exs LICENSE README.md mix.exs lib),
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url}
     ]
+  end
+
+  defp deps do
+    []
   end
 end
