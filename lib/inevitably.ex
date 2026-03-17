4c79e8c1c43d5358e9ba6e6eaa0f53d585782e02
Nicholas Moen <arcanemachine@gmail.com>
2026-03-16 20:22:30 -0600

    chore: scaffold inevitably mix project with MIT license

---
+++ lib/inevitably.ex
@@ -0,0 +1,18 @@
+defmodule Inevitably do
+  @moduledoc """
+  Documentation for `Inevitably`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Inevitably.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/inevitably_test.exs
@@ -0,0 +1,8 @@
+defmodule InevitablyTest do
+  use ExUnit.Case
+  doctest Inevitably
+
+  test "greets the world" do
+    assert Inevitably.hello() == :world
+  end
+end
